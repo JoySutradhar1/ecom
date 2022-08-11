8e5beb965e5f31dfd9b33afb1d7022441c756655
JoySutradhar1 <sdjoy49@gmail.com>
2022-08-11 23:18:23 +0600

    homepage

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("opBtn").style.display= "none";
+}
+  
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("opBtn").style.display= "block";
+}
